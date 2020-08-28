d9462a9e0fe06d7c78ab1caa679223fb47ec95de
rwilt <rosie.wilt@gmail.com>
2020-08-28 11:46:32 -0400

    update

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 // Code your solution here!
 
+function printString(myString){
+   console.log(myString[0])
+
+    if (myString.length > 1){
+        let mySubString = myString.substring(1, myString.length)
+        printString(mySubString)
+    } else {
+        console.log('done')
+        return true
+    }
+}
+
+console.log(printString("fun"))
